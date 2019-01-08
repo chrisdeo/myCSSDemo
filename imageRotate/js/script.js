7e7dee1ee62f853fa07aec8308e30aea21034395
Leo <chendyc@yonyou.com>
2019-01-08 17:38:35 +0800

    change the menu & add some features

--- imageRotate/js/script.js
+++ imageRotate/js/script.js
@@ -1,26 +1,50 @@
 var timer = null,
     index = 0,
-    pics = document.getElementsByTagName("li");
+    pics = document.getElementsByTagName("li"),
+    left = document.querySelector('.left'),
+    right = document.querySelector('.right');
+
+left.onclick = function() {
+    if (index === 0) {
+        index = pics.length-1;
+        changeImg();
+        // pics[index].style.display = 'none';
+        // pics[index=pics.length-1].style.display = 'block';
+    } else {
+        --index;
+        changeImg();
+    }
+}
+
+right.onclick = function() {
+    if (index === pics.length -1) {
+        index = 0;
+        changeImg();
+    } else {
+        ++index;
+        changeImg();
+    }
+}
 
 function slideImg() {
     var banner = document.querySelector('.banner');
-    banner.onmouseover = function(){
-        stopAutoPlay();
-    }
+    startAutoPlay();
     banner.onmouseout = function(){
         startAutoPlay();
     }
-    banner.onmouseout();
+    banner.onmouseover = function(){
+        stopAutoPlay();
+    }
 }
 //开始播放轮播图
 function startAutoPlay(){
     timer = setInterval(function(){
         index++;
-        if(index>3){
+        if(index === pics.length){
             index = 0;
         }
         changeImg();
-    },1000);
+    },1500);
 }
 //暂停播放
 function stopAutoPlay(){
